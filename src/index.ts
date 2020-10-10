262945630cdcb3a5c64b685c184863849fc9f344
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-10-10 13:16:15 -0230

    Add starter API with ping route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+import fastify from 'fastify';
+
+const port = Number(process.env.PORT || 8080);
+const server = fastify();
+
+server.get('/ping', async () => `${JSON.stringify({ status: 'ok', date: Date.now() })}\n`);
+
+server.listen(port, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exitCode = 1;
+  } else {
+    console.log(`Server listening at ${address}`);
+  }
+});
