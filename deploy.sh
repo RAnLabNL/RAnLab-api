41c307dfa1fed6ebde662a0da47f47a1a15f1d64
Michael G. Burton <michaelgburton@hotmail.com>
2020-12-03 23:56:49 -0330

    Fixing compilation error and adding TypeScript compilation to the deploy action

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 # Prerequisite Setup Steps:
 #  gcloud artifacts repositories create ranlab-api-mvp --repository-format docker --location northamerica-northeast1 # create the artifact repo
 #  gcloud auth configure-docker northamerica-northeast1-docker.pkg.dev
+
 docker build -t ranlab-mvp-api:latest .
 docker tag ranlab-mvp-api:latest northamerica-northeast1-docker.pkg.dev/ranlab-mvp-295423/ranlab-api-mvp/ranlab-api-mvp:latest
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io
 docker push northamerica-northeast1-docker.pkg.dev/ranlab-mvp-295423/ranlab-api-mvp/ranlab-api-mvp:latest
 gcloud run --platform=managed --region=northamerica-northeast1  deploy ranlab-api-mvp --image northamerica-northeast1-docker.pkg.dev/ranlab-mvp-295423/ranlab-api-mvp/ranlab-api-mvp:latest
+
--- src/endpoints/filters.ts
+++ src/endpoints/filters.ts
@@ -3,20 +3,20 @@ import {DataLayer} from "../database/productionDataLayer";
 
 interface GetFiltersRequest extends RequestGenericInterface {
   Params: {
-    regionId: number
+    regionId: string
   }
 }
 
 export default function createFiltersEndpoint(app: FastifyInstance, dataLayer: DataLayer) {
   app.get<GetFiltersRequest>('/filters',
-    async () => {
+    async (request) => {
       let response = {
         status: "ok",
         date: Date.now(),
         years: <number[] | undefined> []
       }
 
-      response.years = (await dataLayer.getFilters()).years;
+      response.years = (await dataLayer.getFilters(request.params.regionId)).years;
       return JSON.stringify(response);
     }
   );
